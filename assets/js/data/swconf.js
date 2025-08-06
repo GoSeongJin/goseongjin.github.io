97a4498de516a662c4ba938172d83c7a4675eae3
Your Name <you@example.com>
2025-08-06 13:53:18 +0900

    📝 Add KAI 채용조사 글 (2025 하계 워크샵 발표)

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754455770',resources: [
+    cacheName: 'chirpy-1754455972',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
