035ed4aa8cc2321ced191b803782f72634549ee4
Your Name <you@example.com>
2025-08-06 14:43:31 +0900

    🚀 KAI 진로조사 최종 포스트 배포

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754458640',resources: [
+    cacheName: 'chirpy-1754458991',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
